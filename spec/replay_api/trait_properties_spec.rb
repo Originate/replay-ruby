f4b0598881c97696832ca5109e051b4d88236492
bentona <benton.anderson@gmail.com>
2015-01-07 19:03:34 -0800

    remove timestamp from trait events

--- spec/replay_api/trait_properties_spec.rb
+++ spec/replay_api/trait_properties_spec.rb
@@ -3,20 +3,13 @@ require 'spec_helper'
 module ReplayApi
   describe TraitProperties do
     subject(:properties) { TraitProperties.new }
-    let(:clock) { double :clock, now: Time.now }
-    before { TraitProperties.clock = clock }
-    after { TraitProperties.clock = nil }
-
+    
     describe '#address' do
       specify { expect(properties.address).to be_a(ReplayApi::Address) }
     end
 
     describe '#past_event' do
       specify { expect(properties.past_event).to eq 0 }
-    end
-
-    describe '#timestamp' do
-      specify { expect(properties.timestamp).to eq clock.now.to_i }
     end
   end
 end
